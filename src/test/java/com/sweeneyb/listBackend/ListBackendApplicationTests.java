5d5ee10a2398bb32dd755b1d4905c7155b917992
Brian <tab126@gmail.com>
2018-04-15 20:28:00 -0500

    add a test on add/delete

--- src/test/java/com/sweeneyb/listBackend/ListBackendApplicationTests.java
+++ src/test/java/com/sweeneyb/listBackend/ListBackendApplicationTests.java
@@ -1,16 +1,36 @@
 package com.sweeneyb.listBackend;
 
+import com.sweeneyb.listBackend.dto.ListItem;
+import com.sweeneyb.listBackend.dto.ListItemRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import static org.assertj.core.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ListBackendApplicationTests {
 
+	@Autowired
+	ListItemRepository repo;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testAddAndDelete() {
+		ListItem item = new ListItem("test item");
+		LocalDateTime id = item.getId();
+		repo.save(item);
+		assertThat(repo.existsById(id)).isTrue();
+
+		repo.deleteById(id);
+		assertThat(repo.existsById(id)).isFalse();
 	}
 
 }
